13f8617ab330fda5c3d9388f80619a2f05e5e2d5
kalumemmanueljohn-blip <kalumemmanueljohn@gmail.com>
2026-05-24 11:04:44 +0100

    Update settings.py

--- conference_booking/settings.py
+++ conference_booking/settings.py
@@ -121,14 +121,16 @@ LOGOUT_REDIRECT_URL = '/login/'
 # ====================
 # 📧 EMAIL (Gmail)
 # ====================
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kalumemmanueljohn@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'gnti fexq eidh nobn')
-DEFAULT_FROM_EMAIL = 'kalumemmanueljohn@gmail.com'
-CONTACT_EMAIL = 'kalumemmanueljohn@gmail.com'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONTACT_EMAIL = EMAIL_HOST_USER
 
 # ====================
 # 🔧 CUSTOM SETTINGS
